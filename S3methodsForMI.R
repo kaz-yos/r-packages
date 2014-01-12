fe5b45acd3218697e9fd3e6848c85934d68ea12a
kaz-yos <kazky@mac.com>
2014-01-12 15:48:00 -0500

    first

---
+++ S3methodsForMI.R
@@ -0,0 +1,33 @@
+## print method
+print.MI <- function(object) {
+
+    ## Work on one element at a time
+    sapply(object,
+           function(imp) {
+
+               print(imp$result)
+               cat("--------------------------------------------------------------------------------\n")
+           })
+    ## No meaningful return value
+    return(invisible(NULL))
+}
+## Example
+print(modelMiZelig)
+
+
+## coef method
+coef.MI <- function(object, simplify = FALSE) {
+
+    ## Work on one element at a time
+    sapply(object,
+           function(imp) {
+
+               ## Extract coefficients from the model
+               coef(imp$result)
+           },
+           ## Determine if the result should be simplified to a matrix
+           simplify = simplify)
+}
+## Examples
+coef(modelMiZelig, simplify = TRUE)
+coef(modelMiZelig)
